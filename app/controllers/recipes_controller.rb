c85374fdbd7270c36dfb089e0a4a13e5b9bdff6c
strangeal <bajerijustice@gmail.com>
2023-02-06 07:42:45 -0800

    Add recipes show view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,9 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
   end
+
+  def show
+    @recipes = Recipe.find(params[:id])
+    @foods = Food.all
+  end
 end
