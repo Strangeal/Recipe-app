0dd2f3e83ae39bd1ed92d6e08b2d3d7f6eb611f6
strangeal <bajerijustice@gmail.com>
2023-02-06 09:19:05 -0800

    Add general shoping list index view

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,5 +1,5 @@
 class GeneralShoppingListsController < ApplicationController
   def index
-    
+    @recipe_foods = RecipeFood.includes(:food)
   end
 end
--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -2,6 +2,4 @@ class PublicRecipesController < ApplicationController
   def index
     @recipes = Recipe.includes(:user).where(public: true)
   end
-
-  def show; end
 end
