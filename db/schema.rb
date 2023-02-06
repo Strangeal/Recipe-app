92b0dc258aac68fded77a4267dfef1f902b09c7c
nahnahAJ <nahnahahjuwah@gmail.com>
2023-02-06 12:06:57 +0000

    Run migration

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_06_100501) do
     t.string "preparation_time"
     t.string "cooking_time"
     t.text "description"
-    t.boolean "public"
+    t.boolean "public", default: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
