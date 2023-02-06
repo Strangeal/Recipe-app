b0bfb5a38f2a125d2470f88f7995d05f626b3d1d
strangeal <bajerijustice@gmail.com>
2023-02-06 14:32:48 -0800

    Add changes in recipe view

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,23 @@
 class FoodsController < ApplicationController
   def index
-    @foods = Food.all
+    @foods = Food.includes(:user)
   end
 
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(food_params)
+      if @food.save
+        redirect_to root_path
+      else
+        render :new, alert: 'Sorry an error occured. Please try again'
+      end
+  end
+
+  private
+  def food_params
+    params.permit(:name, :measurement_unit, :price, :quantity)
+  end
 end
