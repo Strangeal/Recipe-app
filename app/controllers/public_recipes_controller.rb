57ccc6675a9f06a73ed500ab715cf377008ee850
strangeal <bajerijustice@gmail.com>
2023-02-06 23:59:08 -0800

    Add routes to recipes details page

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,5 +1,5 @@
 class PublicRecipesController < ApplicationController
   def index
-    @recipes = Recipe.includes(:user).where(public: true)
+    @recipes = Recipe.includes(:user).where(public: true).order(created_at: :desc)
   end
 end
