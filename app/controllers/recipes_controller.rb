a3640bd39d3b056ae98d045df77a9fda3fc71823
strangeal <bajerijustice@gmail.com>
2023-02-07 13:42:47 -0800

    Add alert message in recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,9 +11,10 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new(name: params[:name], preparation_time: params[:preparation_time],
       cooking_time: params[:cooking_time], description: params[:description], public: params[:public])
     @recipe.user = current_user
-    return unless @recipe.save
-
+    if @recipe.save
     redirect_to recipes_path, notice: 'Recipe created successfully'
+    else
+      render :new, alert: 'Sorry an error occured. Please try again'
   end
 
   def show
