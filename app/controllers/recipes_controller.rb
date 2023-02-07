3a229aa96363c89a511a16af0e23e2c4cba1be59
strangeal <bajerijustice@gmail.com>
2023-02-07 00:58:43 -0800

    Add destroy method for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,4 +7,10 @@ class RecipesController < ApplicationController
     @recipes = Recipe.find(params[:id])
     @foods = Food.all
   end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path, alert: 'Recipe delete successfully'
+  end
 end
