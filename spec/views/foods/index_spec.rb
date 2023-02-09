0df79809b31ff3811f066ef8d191c52a7477f897
strangeal <bajerijustice@gmail.com>
2023-02-09 07:07:46 -0800

    Add integration specs for foods index view

---
+++ spec/views/foods/index_spec.rb
@@ -0,0 +1,69 @@
+require 'rails_helper'
+
+RSpec.describe 'Food index view', type: :system do
+  describe 'Index page' do
+    before(:each) do
+      @user_1 = User.create(name: "Tom")
+      @user_2 = User.create(name: "Dianne")
+      @food_1 = Food.create(name: "Apple", measurement_unit: "5kg", price: 1.99, quantity: 3, user_id: 1)
+      @food_2 = Food.create(name: "Pizza", measurement_unit: "5kg", price: 75, quantity: 1, user_id: 2)
+
+      @foods = Food.all
+    end
+
+    it 'Can see the names of each food' do
+      visit root_path
+      @foods.each do |food|
+        expect(page).to have_content(food.measurement_unit.to_s)
+      end
+    end
+
+    it 'Can see the names of each food' do
+      visit root_path
+      @foods.each do |food|
+        expect(page).to have_content(food.name.to_s)
+      end
+    end
+
+    it 'Can see the price of each food' do
+      visit root_path
+      @foods.each do |food|
+        expect(page).to have_content(food.price)
+      end
+    end
+
+    it 'Can see the quantity of each food' do
+      visit root_path
+      @foods.each do |food|
+        expect(page).to have_content(food.quantity)
+      end
+    end
+
+    it 'Can see a button that lets me add new food' do
+      visit root_path
+      expect(page).to have_content('Add Food')
+    end
+
+    it 'Can click on Add Food, to be redirected to a form to add new food' do
+      visit root_path
+      click_link 'Add Food'
+      expect("#{page.current_url}foods/new").to match new_food_path
+    end
+
+  end
+end
+
+
+
+
+
+# require 'rails_helper'
+
+# RSpec.describe 'Food index view', type: :system do
+#   describe 'Index page' do
+#     before(:each) do
+      
+#     end
+#   end
+
+# end
---
+++ spec/views/recipes/index_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe 'Recipe index view', type: :system do
+  describe 'Index page' do
+    before(:each) do
+      @user_1 = User.create(name: "Tom")
+      @user_2 = User.create(name: "Dianne")
+      @recipe_1 = Recipe.create(name: "Recipe 3", preparation_time: "12:05", cooking_time: "6:45", description: "Lorem ipsum dolor sit", user_id: 1)
+      @recipe_2 = Recipe.create(name: "Cheeseburger Soup", preparation_time: "12:05", cooking_time: "6:45", description: "Lorem ipsum dolor sit", user_id: 2, public: true)
+
+      
+      @recipes = Recipe.all
+      visit recipes_path
+    end
+
+    it 'Can see the names of each recipe' do
+      @recipes.each do |recipe|
+        expect(page).to have_content(recipe.name.to_s)
+      end
+    end
+
+    it 'Can see the names of each recipe' do
+      @recipes.each do |recipe|
+        expect(page).to have_content(recipe.description.to_s)
+      end
+    end
+    
+    it 'Can see a button that lets me Add New Recipe' do
+      expect(page).to have_content('Add New Recipe')
+    end
+
+    it 'Can click on Add New Recipe, to be redirected to a form to Add New Recipe' do
+      visit recipes_path
+      click_link "#{@recipe_1.name}"
+      expect("#{page.current_url}recipes/#{@user_1}").to match recipe_path(@user_1)
+    end
+
+
+  end
+
+end
